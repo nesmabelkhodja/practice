af27bf3097164194f6f0bbaea497a52f09b38aef
Nesma Belkhodja <nb2253@nyu.edu>
2019-12-05 16:01:45 -0500

    account for two players

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
+function Square(props) {
     return (
       <button 
         className="square" 
-        onClick={() => this.props.onClick()}
+        onClick={props.onClick}
       >
-        {this.props.value}
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
@@ -20,13 +18,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -39,7 +41,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+
+    (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
