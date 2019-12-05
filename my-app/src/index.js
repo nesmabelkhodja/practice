134babcdafe373e54907ee239b529dda7a096a10
Nesma Belkhodja <nb2253@nyu.edu>
2019-12-05 15:52:27 -0500

    render squares

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,10 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={function(){alert('click');}}>
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}
+      >
         {this.props.value}
       </button>
     );
@@ -13,8 +16,26 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+    <Square 
+    value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+     />
+    );
   }
 
   render() {
