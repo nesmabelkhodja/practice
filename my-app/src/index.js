70fc5ea9908f4bdbb225344cb81c0330c175bb9d
Nesma Belkhodja <nb2253@nyu.edu>
2019-12-10 21:53:52 -0500

    formatting

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -153,10 +153,9 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      pos = lines[i];
+     // pos = lines[i];
       return squares[a];
     }
   }
   return null;
 }
-
